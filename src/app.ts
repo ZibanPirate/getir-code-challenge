868def18d807fe6dc9a549b304c7a91b9565c37d
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-12-06 16:38:01 +0400

    added async error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
+// For asynchronous error heralding
+import "express-async-errors";
 // db setup
 import "./services/mongodb";
 
+import express, { RequestHandler } from "express";
+
 import { errorHandler } from "./middleware/error-handler";
-import express from "express";
 import { healthController } from "./controllers/health";
 import { json } from "body-parser";
 import { recordsController } from "./controllers/records";
@@ -15,5 +18,8 @@ app.use(json());
 // Controllers
 app.use("/", healthController);
 app.use("/records", recordsController);
+
+// Fallback middleware setup
+app.use((errorHandler as unknown) as RequestHandler);
 
 export { app };
---
+++ src/middleware/error-handler/index.ts
@@ -0,0 +1,23 @@
+import { ErrorRequestHandler } from "express";
+import { GeneralResponse } from "../../types";
+
+export const errorHandler: ErrorRequestHandler<
+  never,
+  GeneralResponse,
+  unknown
+> = async (err, req, res, next) => {
+  // Logs error
+  console.log("🚩 Internal Server Error");
+  console.log(err);
+
+  // Skip if headers are already sent
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  // return a general error response
+  res.status(500).json({
+    code: 500,
+    msg: err.message || "Internal server error",
+  });
+};
