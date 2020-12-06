cb6dc72c33f63db56af12e247c8d021a646d2fc1
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-12-06 20:27:48 +0400

    added general security rules

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,26 @@ import "./services/mongodb";
 
 import express, { RequestHandler } from "express";
 
+import cors from "cors";
 import { errorHandler } from "./middleware/error-handler";
 import { healthController } from "./controllers/health";
+import helmet from "helmet";
 import { json } from "body-parser";
+import rateLimit from "express-rate-limit";
 import { recordsController } from "./controllers/records";
 
 const app = express();
 
 // Global middleware setup
+app.use(helmet());
 app.use(json());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }),
+);
+app.use(cors());
 
 // Controllers
 app.use("/", healthController);
